b3e78fa10434f0bbbd6c546dfd50b18fa1c60b69
Nama Anda <daffaibrani7@gmail.com>
2026-05-11 14:49:07 +0700

    feat: apply real firebase config for cloud sync

--- script.js
+++ script.js
@@ -19,13 +19,14 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- Firebase Initialization ---
     // PASTE YOUR FIREBASE CONFIG HERE
     const firebaseConfig = {
-        apiKey: "YOUR_API_KEY",
-        authDomain: "YOUR_AUTH_DOMAIN",
-        databaseURL: "YOUR_DATABASE_URL",
-        projectId: "YOUR_PROJECT_ID",
-        storageBucket: "YOUR_STORAGE_BUCKET",
-        messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-        appId: "YOUR_APP_ID"
+        apiKey: "AIzaSyBIYyNVsi1AqUgvafEk183OAuswbKTVMGQ",
+        authDomain: "bekantans.firebaseapp.com",
+        databaseURL: "https://bekantans-default-rtdb.asia-southeast1.firebasedatabase.app/",
+        projectId: "bekantans",
+        storageBucket: "bekantans.firebasestorage.app",
+        messagingSenderId: "261202389756",
+        appId: "1:261202389756:web:1bc231f87a45d3cd624579",
+        measurementId: "G-G71HS3VVQC"
     };
 
     // Initialize Firebase if not already initialized
